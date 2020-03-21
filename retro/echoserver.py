6e6b5de77e85bb9cf949238dfaff002f30cef646
GBisi <giuseppe.bisicchia@outlook.it>
2020-03-21 14:54:56 +0100

    echoserver run forever

--- retro/echoserver.py
+++ retro/echoserver.py
@@ -5,33 +5,25 @@ from mysocket import MySocket
 class EchoServer:
     
     def __init__(self, ip, port, n=1, m=1, header = None, debug = False):
-
         if header is None:
-            
             self.header = "\nEchoed by "+str(port)
-
         else:
             self.header = header
-
         self.socket = MySocket(port,n,m, ip=ip)
         self.debug = debug
         print("--- ECHO SERVER ONLINE AT PORT "+str(port)+" ---")
 
 
-    def start(self, delta):
+    def start(self, delta=0):
         print("--- ECHO SERVER START ---")
         self.socket.start()
         start = time.time()
-        while time.time()-start < delta:
-
+        while delta == 0 or time.time()-start < delta:
             msg = self.socket.receive()
-            
-
             if msg is not None:
                 if self.debug:
                     print(msg)
                 self.socket.send(str(msg.get_data())+self.header,msg.get_sender())
-
         print("--- ECHO SERVER CLOSE ---")
     
 if __name__ == "__main__":
