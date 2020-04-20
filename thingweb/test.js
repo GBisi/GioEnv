6ec1dc5b67a28c54ed1669179241b0721ee7ba04
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-20 09:02:04 +0200

    room test

--- thingweb/test.js
+++ thingweb/test.js
@@ -1,26 +1,3 @@
-/********************************************************************************
- * Copyright (c) 2018 - 2020 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
- * Document License (2015-05-13) which is available at
- * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
- *
- * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
- ********************************************************************************/
-
-// This is an example Thing script. 
-// It has a count property that can be incremented or decremented via actions and its changes are reported via events.
-// It also has two properties that return an image. The SVG property is also influenced by the increment and decrement actions.
-// Features
-// * basic properties, actions, events
-// * uriVariables
-// * multi-language
-// * image contentTypes for properties (Note: the contentType applies to all forms of the property)
 
 //https://github.com/lancaster-university/microbit-dal/blob/master/source/core/MicroBitDevice.cpp
 function getFriendlyName(serial_number){
@@ -61,7 +38,7 @@ function newMicrobit(serial){
         "@context": "https://www.w3.org/2019/wot/td/v1",
         properties: {
             serial_number: {
-                type: "number",
+                type: "float",
                 description: "This Microbit's serial number",
                 descriptions: {
                     "it": "Numero seriale del Microbit"
@@ -102,176 +79,134 @@ function newMicrobit(serial){
             
 }
 
-WoT.produce({
-    title: "counter",
-    titles: {
-        "en": "counter",
-        "de": "zähler",
-        "it": "Contatore"
-    },
-    description: "counter example Thing",
-    descriptions: {
-        "en": "counter example Thing",
-        "de": "Zähler Beispiel Ding",
-        "it": "Contatore Esempio"
-    },
-    support: "git://github.com/eclipse/thingweb.node-wot.git",
-    "@context": ["https://www.w3.org/2019/wot/td/v1", { "iot": "http://example.org/iot" }],
-    properties: {
-        count: {
-            type: "integer",
-            description: "current counter value",
-            descriptions: {
-                "en": "current counter value",
-                "de": "Derzeitiger Zähler Stand",
-                "it": "valore attuale del contatore"
-            },
-            "iot:Custom": "example annotation",
-            observable: true,
-            readOnly: true
+function addParams(thing, name, thresholds, labels, description = "", descriptionIta = ""){
+
+    thing["properties"][name] = {
+        type:"number",
+        description: description,
+        descriptions: {
+            "it": descriptionIta
         },
-        countAsImage: {
-            description: "current counter value as SVG image",
-            forms: [{
-                    contentType: "image/svg+xml"
-                }],
-            observable: false,
-            readOnly: true
+        observable: true,
+        readOnly: true
+    }
+
+    thing["properties"][name+"L"] = {
+        type:"string",
+        description: description+" in levels",
+        descriptions: {
+            "it": descriptionIta+" in livelli"
         },
-        redDotImage: {
-            description: "Red dot image as PNG",
-            forms: [{
-                    contentType: "image/png"
-                }],
-            observable: false,
-            readOnly: true
+        observable: true,
+        readOnly: true,
+        enum:labels,
+    }
+
+    handler = (thing) => {
+        return new Promise((resolve, reject) => {
+            return thing.readProperty("temp").then((val) => {
+
+                value = labels[thresholds.length]
+
+                for(var i=0; i<thresholds.length; i++){
+                    if(val < thresholds[i]){
+                        value = labels[i]
+                        break
+                    }
+                }
+                
+                old = thing.readProperty(name+"L")
+                if(value != old){
+                     thing.writeProperty(name+"L", value);
+                }
+
+                thing.writeProperty("last_indoor_update", (new Date()).toISOString());
+            });
+        });
+    };
+
+    return handler
+
+}
+
+function getRoom(id){
+    room = {
+        title: "room"+id,
+        description: "A Smart Room",
+        descriptions: {
+            "it": "Una stanza intelligente"
         },
-        lastChange: {
-            type: "string",
-            description: "last change of counter value",
-            descriptions: {
-                "en": "last change of counter value",
-                "de": "Letzte Änderung",
-                "it": "ultima modifica del valore"
-            },
-            observable: true,
-            readOnly: true
-        }
-    },
-    actions: {
-        increment: {
-            description: "Incrementing counter value (with optional step parameter as uriVariable)",
-            descriptions: {
-                "en": "increment value",
-                "de": "Zähler erhöhen",
-                "it": "incrementare valore"
-            },
-            uriVariables: {
-                step: { "type": "integer", "minimum": 1, "maximum": 250 }
-            }
+        "@context": "https://www.w3.org/2019/wot/td/v1",
+        properties: {
+            last_indoor_update: {
+                    type: "string",
+                    description: "Last room's sensors update",
+                    descriptions: {
+                        "it": "Ultimo aggiornamento dai sensori nella stanza"
+                    },
+                    observable: true,
+                    readOnly: true
+                },
+                
+            last_otdoor_update: {
+                    type: "string",
+                    description: "Last outdoor update",
+                    descriptions: {
+                        "it": "Ultimo aggiornamento dei parametri esterni"
+                    },
+                    observable: true,
+                    readOnly: true
+                }
         },
-        decrement: {
-            description: "Decrementing counter value (with optional step parameter as uriVariable)",
+        
+        events: {
+        fix: {
+            description: "Some action to do",
             descriptions: {
-                "en": "decrement value",
-                "de": "Zähler verringern",
-                "it": "decrementare valore"
-            },
-            uriVariables: {
-                step: { "type": "integer", "minimum": 1, "maximum": 250 }
-            }
-        },
-        reset: {
-            description: "Resetting counter value",
-            descriptions: {
-                "en": "Resetting counter value",
-                "de": "Zähler resettieren",
-                "it": "resettare valore"
-            }
-        }
-    },
-    events: {
-        change: {
-            description: "change event",
-            descriptions: {
-                "en": "change event",
-                "de": "Änderungsnachricht",
-                "it": "resettare valore"
+                "it": "Qualche azione da compiere"
             }
         }
     }
-})
-    .then((thing) => {
-    console.log("Produced " + thing.getThingDescription().title);
-    // init property values
-    thing.writeProperty("count", 0);
-    thing.writeProperty("lastChange", (new Date()).toISOString());
-    thing.setPropertyReadHandler("countAsImage", () => {
-        return thing.readProperty("count").then((count) => {
-            return new Promise((resolve, reject) => {
-                resolve("<svg xmlns='http://www.w3.org/2000/svg' height='30' width='200'>" +
-                    "<text x='0' y='15' fill='black'>" + count + "</text>" +
-                    "</svg>");
-            });
+    };
+
+    handlers = {}
+
+    handlers["temp"]=addParams(room,"temp",[18,20,22,24], ["VERY_LOW","LOW","MEDIUM","HIGH","VERY_HIGH"],"Room's temperature","Temperatura della stanza");
+    handlers["light"]=addParams(room,"light",[25,80], ["LOW","MEDIUM","HIGH"],"Room's light","Luminosita' della stanza");
+    handlers["time"]=addParams(room,"time",[7,13,19,22], ["NIGHT","MORNING","AFTERNOON","EVENING","NIGHT"],"Time","Orario");
+    
+	handlers["outdoor_light"]=addParams(room,"outdoor_light",[25,80], ["LOW","MEDIUM","HIGH"], "Outdoor light","Luminosita' esterna")
+	handlers["outdoor_temp"]=addParams(room,"outdoor_temp",[18,20,22,24], ["VERY_LOW","LOW","MEDIUM","HIGH","VERY_HIGH"], "Outdoor temperature","Temperatura esterna")
+    
+    return room,handlers
+}
+
+function newRoom(id){
+    room,handlers = getRoom(id)
+    WoT.produce(room).then((thing) => {
+            
+            thing.setPropertyWriteHandler("temp", handlers["temp"]);
+            
+            thing.writeProperty("temp", 0);
+            thing.writeProperty("light", 0);
+            thing.writeProperty("time", (new Date()).toISOString());
+
+            thing.writeProperty("ourdoor_temp", 0);
+            thing.writeProperty("outdoor_light", 0);
+
+            thing.writeProperty("last_indoor_update", 0);
+            thing.writeProperty("last_outdoor_update", 0);
+
+            thing.expose().then(() => { console.info(`${thing.getThingDescription().title} ready!`); });
+        })
+        .catch((e) => {
+            console.log(e);
         });
-    });
-    thing.setPropertyReadHandler("redDotImage", () => {
-        return new Promise((resolve, reject) => {
-            // data:image/png;base64,
-            resolve("iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==");
-        });
-    });
-    // set action handlers
-    thing.setActionHandler("increment", (params, options) => {
-        return thing.readProperty("count").then((count) => {
-            let step = 1;
-            console.log(options);
-            if (options && typeof options === 'object' && 'uriVariables' in options) {
-                if ('step' in options['uriVariables']) {
-                    let uriVariables = options['uriVariables'];
-                    step = uriVariables['step'];
-                }
-            }
-            let value = count + step;
-            console.log("Incrementing count from " + count + " to " + value + " (with step " + step + ")");
-            thing.writeProperty("count", value);
-            thing.writeProperty("lastChange", (new Date()).toISOString());
-            thing.emitEvent("change", value);
-        });
-    });
-    thing.setActionHandler("decrement", (params, options) => {
-        return thing.readProperty("count").then((count) => {
-            let step = 1;
-            if (options && typeof options === 'object' && 'uriVariables' in options) {
-                if ('step' in options['uriVariables']) {
-                    let uriVariables = options['uriVariables'];
-                    step = uriVariables['step'];
-                }
-            }
-            let value = count - step;
-            console.log("Decrementing count from " + count + " to " + value + " (with step " + step + ")");
-            thing.writeProperty("count", value);
-            thing.writeProperty("lastChange", (new Date()).toISOString());
-            thing.emitEvent("change", value);
-        });
-    });
-    thing.setActionHandler("reset", () => {
-        return new Promise((resolve, reject) => {
-            console.log("Resetting count");
-            thing.writeProperty("count", 0);
-            thing.writeProperty("lastChange", (new Date()).toISOString());
-            thing.emitEvent("change", 0);
-            resolve();
-        });
-    });
-    // expose the thing
-    thing.expose().then(() => { console.info(thing.getThingDescription().title + " ready"); });
-})
-    .catch((e) => {
-    console.log(e);
-});
+}
+
+newRoom(129)
 
 newMicrobit(384933164);
-newMicrobit(1252840479.9999999);
-newMicrobit(671265031);
-newMicrobit(20458004765.9999998);
+//newMicrobit(1252840479.9999999);
+//newMicrobit(671265031);
+//newMicrobit(20458004765.9999998);
