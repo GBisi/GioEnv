a1461d69f806da7a2eb3a05a3d498ad0eaa56d12
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-20 10:33:35 +0200

    fix

--- thingweb/test.js
+++ thingweb/test.js
@@ -122,6 +122,7 @@ function addParams(thing, name, thresholds, labels, description = "", descriptio
                 }
 
                 thing.writeProperty("last_indoor_update", (new Date()).toISOString());
+                resolve()
 
             });
         });
