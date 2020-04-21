fc8811706266721f0b9853fdcacda63742d420df
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-21 22:53:05 +0200

    fix?

--- thingweb/new-thingweb/packages/binding-file/src/file-client.ts
+++ thingweb/new-thingweb/packages/binding-file/src/file-client.ts
@@ -56,6 +56,12 @@ export default class FileClient implements ProtocolClient {
     });
   }
 
+  public updateResource(form: Form, content: Content): Promise<any> {
+    return new Promise<Object>((resolve, reject) => {
+      reject(new Error(`FileClient does not implement update`));
+    });
+  }
+
   public writeResource(form: Form, content: Content): Promise<any> {
     return new Promise<Object>((resolve, reject) => {
       reject(new Error(`FileClient does not implement write`));
