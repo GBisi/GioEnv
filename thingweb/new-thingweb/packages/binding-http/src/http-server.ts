3996e995cfe2d2c0481200199a34a40b1500a44a
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-25 20:25:27 +0200

    test

--- thingweb/new-thingweb/packages/binding-http/src/http-server.ts
+++ thingweb/new-thingweb/packages/binding-http/src/http-server.ts
@@ -479,10 +479,8 @@ export default class HttpServer implements ProtocolServer {
         res.setHeader("Content-Type", ContentSerdes.DEFAULT);
         res.writeHead(201);
         req.on("data", (data) => {
-          console.debug(data);
-          console.debug();
           let script = this.TDtoScript(JSON.parse(data))
-          //sthis.servient.runPrivilegedScript(script); 
+          this.servient.runPrivilegedScript(script); 
           res.end(script);});
       }else {
         respondUnallowedMethod(res, "GET, POST");
