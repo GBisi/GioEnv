c136c44905c8ca37f24f927caa6692207911fcd6
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-28 00:03:02 +0200

    test

--- thingweb/new-thingweb/packages/binding-http/src/http-server.ts
+++ thingweb/new-thingweb/packages/binding-http/src/http-server.ts
@@ -475,7 +475,7 @@ if("properties" in description["handlers"]){
     let contentType: string = Array.isArray(contentTypeHeader) ? contentTypeHeader[0] : contentTypeHeader;
 
     req.on("data", (data) => {
-      console.log(data.toString("utf-8"))
+      console.log("DATA: "+JSON.stringify(data))
     });
 
     if (req.method === "PUT" || req.method === "POST") {
