0136c5a6c66025ceb0de5d4717656e9e70d606dd
GBisi <giuseppe.bisicchia@outlook.it>
2020-04-30 16:07:53 +0200

    Added users rules

--- thingweb/MicrobitManager/microbitmanager.py
+++ thingweb/MicrobitManager/microbitmanager.py
@@ -11,14 +11,13 @@ import configparser
 
 from serial import Serial, SerialException
 
-class ClientManager:
+class MicrobitManager:
 
     def __init__(self, serialport, clientport, server, ip="127.0.0.1"):
         self.MICROBIT_PORT = serialport
         self.CLIENT_PORT = clientport
         self.SERVER = "http://"+server[0]+":"+str(server[1])+"/"
-
-        self.microbit = {}
+        self.microbits = {}
         self.rooms = {}
 
         print("MICROBIT MANAGER ONLINE @ "+ip+":"+str(clientport))
@@ -32,9 +31,11 @@ class ClientManager:
                 while True:
                     try:
                         byte = s.readline()
+                        if byte is None or byte == b'':
+                            continue
                         byte = byte.decode().strip()
                         line = json.loads(byte)
-                        print("Serial:",line["s"],line["n"],line["v"])
+                        #print("Serial:",line["s"],line["n"],line["v"])
                         yield int(line["s"]), line["n"], int(line["v"])
                     except ValueError:
                         print("Serial: Value Error:",byte)
@@ -45,43 +46,64 @@ class ClientManager:
         
         try:
             r = requests.patch(self.SERVER+thing+"/properties/"+prop,data=str(val))
+            print("WoT: updated ", thing, r.status_code, r.text)
             return r.status_code,r.text
         except requests.ConnectionError:
-            print("WoT Server: Connection Error")
+            print("WoT Server: Connection Error "+r.status_code)
 
     def add_thing(self, thing):
         try:
             td = thing.get_thing_description()
             r = requests.post(self.SERVER,data=json.dumps(td))
+            if r.status_code != 201:
+                print("WoT: added ",td["thing"]["title"], r.status_code, r.text)
+            else:
+                print("WoT: added ",td["thing"]["title"], r.status_code)
             return r.status_code,r.text
         except requests.ConnectionError:
-            print("WoT Server: Connection Error")
-            exit()
+            print("WoT Server: Connection Error "+r.status_code)
+            return None
+
+    def get_thing(self, thing):
+        try:
+            r = requests.get(self.SERVER+thing)
+            if r.status_code != 200:
+                print("WoT: getted ",thing, r.status_code, r.text)
+            else:
+                print("WoT: getted ",thing, r.status_code)
+            if r.status_code == requests.codes.ok:
+                return r.json()
+            return None
+        except requests.ConnectionError:
+            print("WoT Server: Connection Error "+r.status_code)
+            return None
 
 
     def run(self):
        with open('./rooms.json') as file:
             self.data = json.load(file)
-            
-            for microbit in self.data:
-                for attr in self.data[microbit]:
-                    for n in self.data[microbit][attr]:
-                        if n not in self.rooms:
-                            self.rooms[n] = Room(n)
-                            self.add_thing(self.rooms[n])
 
-            for serial_number, name, val in self.ReadSerial(0):
+            for serial_number, name, val in self.ReadSerial(None):
                 
                 if serial_number not in self.microbits:
                     self.microbits[serial_number] = Microbit(serial_number)
-                    self.add_thing(self.microbits[serial_number])
+                    if self.get_thing(self.microbits[serial_number].get_friendly_name()) is None:
+                        self.add_thing(self.microbits[serial_number])
 
                 microbit = Microbit.get_microbit_name(serial_number)
+                print("Serial: ",microbit,name,val)
+
                 self.update(microbit,name,val)
-                if name in self.data[ microbit]:
-                    for r in self.data[microbit][name]:
-                        print(microbit,r,name)
-                        self.update(self.rooms[r],name,val)
+                if microbit in self.data:
+                    if name in self.data[microbit]:
+                        for r in self.data[microbit][name]:
+                            if r not in self.rooms:
+                                self.rooms[r] = Room(r)
+                            if self.get_thing(self.rooms[r].get_thing_description()["thing"]["title"]) is None:
+                                if self.add_thing(self.rooms[r]) is None:
+                                    print("Impossible to connect with Room "+r)
+                                    continue;
+                            self.update(self.rooms[r].get_thing_description()["thing"]["title"],name,val)
 
 
 # python Desktop/WoT/clientmanager.py COM7 4201 127.0.0.1 4200
@@ -104,7 +126,7 @@ def configuration():
     SERIAL_PORT = config["CLIENTMANAGER"]["SERIAL_PORT"]
     CLIENT_PORT = int(config["CLIENTMANAGER"]["CLIENT_PORT"])
 
-    ClientManager(SERIAL_PORT,int(CLIENT_PORT),(str(SERVER_IP),int(SERVER_PORT)), ip=MY_IP).run()
+    MicrobitManager(SERIAL_PORT,int(CLIENT_PORT),(str(SERVER_IP),int(SERVER_PORT)), ip=MY_IP).run()
 
 if __name__ == "__main__":
     
@@ -112,4 +134,4 @@ if __name__ == "__main__":
         print("microbitmanager [serial_port] [my_ip] [my_port] [server_ip] [server_port]")
         configuration()
     else:
-        ClientManager(str(sys.argv[1]),int(sys.argv[3]),(str(sys.argv[4]),int(sys.argv[5])), ip=sys.argv[2]).run()
+        MicrobitManager(str(sys.argv[1]),int(sys.argv[3]),(str(sys.argv[4]),int(sys.argv[5])), ip=sys.argv[2]).run()
